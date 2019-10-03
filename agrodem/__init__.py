49145f6aeed535b04da4d8e8deac78bc303cd131
Camilo Ramirez Gomez <camilorg@UG.KTH.SE>
2019-10-03 16:42:18 +0200

    added __init__ file and initial import statements to it

---
+++ agrodem/__init__.py
@@ -0,0 +1,12 @@
+##packages imports###
+import pandas as pd
+import numpy as np
+import math
+import pyeto
+
+##modules imports##
+from .water_demand import *
+
+math.exp = np.exp
+math.pow = np.power
+math.sqrt = np.sqrt
