ef9c72ca8a91a9f6ce222722d327274a467086c7
Camilo Ramirez Gomez <camilorg@UG.KTH.SE>
2020-01-24 11:46:26 +0100

    changes to css structure

--- dashboard/app.py
+++ dashboard/app.py
@@ -87,7 +87,8 @@ layout = dict(
 )
 
 app = dash.Dash(__name__,
-                external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://use.fontawesome.com/releases/v5.12.0/css/all.css'],
+                external_stylesheets=[dbc.themes.BOOTSTRAP,
+                                      'https://use.fontawesome.com/releases/v5.12.0/css/all.css'],
                 # these meta_tags ensure content is scaled correctly on different devices
                 # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
                 meta_tags=[
